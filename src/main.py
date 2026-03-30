3c51a268b5fad9389cdbca131357ad27670842d7
Vicious66666 <jim@thedatfactory.com>
2026-03-30 16:38:20 -0400

    CHAPTER 5 LAST LESSON

--- src/main.py
+++ src/main.py
@@ -11,9 +11,12 @@ def main():
         basepath = sys.argv[1] 
     else:
         basepath = "/"  
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    copy_directory_recursive("static", "public")
+    #if os.path.exists("public"):
+    #    shutil.rmtree("public")
+    #copy_directory_recursive("static", "public")
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")   
+    copy_directory_recursive("static", "docs")
     #generate_page("content/index.md", "template.html", "public/index.html")
     #generate_pages_recursive("content", "template.html", "public") 
     print("basepath=", basepath)
